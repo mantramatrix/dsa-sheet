f07b39246df086c00d7b0f8e29b62bd640458212
Aditya Pant <adityapant10f@gmail.com>
2026-08-20 15:52:35 +0530

    pattern 14 to 17

--- pattern15.cpp
+++ pattern15.cpp
@@ -1,19 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void pattern14(int n){
-    for(int i=0 ; i<n ; i++){
-        for(char ch = 'A'; ch <= 'A' + i;ch++){
+void pattern15(int n){
+    for(int i=0;i<n;i++){
+        for(char ch= 'A'; ch <= 'A' + (n-i-1);ch++){
             cout << ch << " ";
         }
         cout << endl;
-        
     }
-
 }
 
-int main() {
+int main(){
     int n;
     cin >> n;
-    pattern14(n);
+    pattern15(n);
 }
